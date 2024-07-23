2fff0897157e5c1b54f70c0a4b11e8b8dd2a76e3
BrenoLemesD <brenolemesd@gmail.com>
2024-07-23 19:59:56 -0300

    feat: feat: creates Hero component UI

--- src/theme.ts
+++ src/theme.ts
@@ -3,7 +3,7 @@ import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 let  theme = createTheme({
   palette: {
     primary: {
-      main: '#000000',
+      main: '#191919',
     },
     secondary: {
       main: '#4f8e3e',
